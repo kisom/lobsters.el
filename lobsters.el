3f50d410b7d109854bf3e8bca6668e3b048d4b88
Kyle Isom <coder@kyleisom.net>
2013-04-02 10:34:32 -0600

    update package header

--- lobsters.el
+++ lobsters.el
@@ -1,27 +1,29 @@
 ;;; lobsters.el -- view the front page of lobste.rs in an emacs buffer
 ;; Copyright (C) 2013 Kyle Isom
 
-;; Author: Kyle Isom
-;; URL: http://github.com/kisom/lobsters.el
-;; Version: 1.0
-;; Created: 2013-04-01
+;;; Author: Kyle Isom
+;;; URL: http://github.com/kisom/lobsters.el
+;;; Version: 1.0
+;;; Created: 2013-04-01
 
 ;; This file is NOT part of GNU Emacs.
 
-;; This package provides the 'lobsters' command, which will
+;; This package provides the 'lobste.rs' command, which will
 ;; display the current lobste.rs front page in a new emacs buffer.
 ;; The buffer can be closed with the '
 
-;;; Install
+;;; Installation
 
 ;; Copy into your emacs source directory, and add
 ;; (autoload 'lobsters "lobsters" "View the lobste.rs home page." t nil)
 ;; to your emacs init file.
 
 ;;; Usage
+;; M-x lobsters displays the front page.
+;; M-x lobste.rs-newest displays the newest posts.
+;; q closes the buffer.
 
-
-;;; License:
+;;; License
 
 ;; Permission to use, copy, modify, and distribute this software for any
 ;; purpose with or without fee is hereby granted, provided that the above
@@ -35,13 +37,13 @@
 ;; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 ;; OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-;;; Code:
 
 (require 'cl)
 (require 'json)
 (require 'url)
 
 (defvar lobsters-feed "https://lobste.rs/rss.json")
+(defvar lobsters-feed-newest "https://lobste.rs/newest.json")
 (defvar lobsters-bufname "lobste.rs")
 
 (defun lobsters-updated-time ()
@@ -49,6 +51,7 @@
                   (format-time-string "%Y-%m-%d %H:%M:%S"
                                       (current-time))
                   "\n\n")))
+
 (defun lobsters-buffer-to-string (b)
   (unless (bufferp b)
     nil)
@@ -70,9 +73,9 @@
    (lobsters-buffer-to-string
     (lobsters-fetch-url lobsters-newest))))
 
-(defun lobsters-fetch ()
+(defun lobsters-fetch (url)
   (let* ((lobsters-buffer (generate-new-buffer "lobsters")))
-    (setq lobsters-buffer (url-retrieve-synchronously lobsters-newest))
+    (setq lobsters-buffer (url-retrieve-synchronously url))
     (save-excursion
       (set-buffer lobsters-buffer)
       (if (string-match "200 OK$" (buffer-string))
@@ -121,7 +124,7 @@
                         'mouse-face 'highlight))
     (insert ")\n")))
 
-(defun lobsters ()
+(defun lobste.rs ()
   (interactive)
   (switch-to-buffer (get-buffer-create lobsters-bufname))
   (local-set-key (kbd "q") (lambda ()
@@ -133,9 +136,27 @@
   (erase-buffer)
   (post-link '((title . "lobste.rs front page") (url . "https://lobste.rs")))
   (insert "--------------------\n\n")
-  (mapcar #'lobsters-post-link (lobsters-fetch))
+  (mapcar #'lobsters-post-link (lobsters-fetch lobsters-feed))
   (lobsters-updated-time)
-  (setq buffer-read-only t))
+  (setq buffer-read-only t)
+  (move-to-window-line 2))
+
+(defun lobste.rs-newest ()
+  (interactive)
+  (switch-to-buffer (get-buffer-create lobsters-bufname))
+  (local-set-key (kbd "q") (lambda ()
+                             (interactive)
+                             (progn
+                               (local-unset-key (kbd "q"))
+                               (kill-buffer (buffer-name)))))
+  (setq buffer-read-only nil)
+  (erase-buffer)
+  (post-link '((title . "newest lobste.rs posts") (url . "https://lobste.rs")))
+  (insert "----------------------\n\n")
+  (mapcar #'lobsters-post-link (lobsters-fetch lobsters-feed-newest))
+  (lobsters-updated-time)
+  (setq buffer-read-only t)
+  (move-to-window-line 2))
 
 (provide 'lobste.rs)
 
